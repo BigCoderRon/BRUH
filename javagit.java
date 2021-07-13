90a571fa0f635f95c29f1c1667b589bc394f9969
BigCoderRon <80131320+BigCoderRon@users.noreply.github.com>
2021-07-13 16:02:40 +0100

    javagit2.java
    Now its better

--- javagit.java
+++ javagit.java
@@ -5,7 +5,7 @@ public class javagit {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("You are using git");
-		System.out.println("You should not see me 0_0");
+		System.out.println("You should not see me ");
 	}
 
 }
