dc387e59be4aea4331332a2b78ef3425b164f57b
BigCoderRon <80131320+BigCoderRon@users.noreply.github.com>
2021-07-13 16:01:50 +0100

    Add files via upload

---
+++ javagit.java
@@ -0,0 +1,11 @@
+package gitjava;
+
+public class javagit {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("You are using git");
+		System.out.println("You should not see me 0_0");
+	}
+
+}
