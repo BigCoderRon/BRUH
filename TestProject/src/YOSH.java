8f1554bf2377241d4c71e67999f688dd677cb080
Ronnel Salgado <salgadoronnel7@gmail.com>
2021-07-13 17:31:00 +0100

    I added a enter name function and removed the hello print :)

--- TestProject/src/YOSH.java
+++ TestProject/src/YOSH.java
@@ -1,9 +1,14 @@
+import java.util.Scanner;
 
 public class YOSH {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-	System.out.println("Hello");
+	Scanner scanner = new Scanner(System.in);
+	
+	System.out.println("enter your name");
+	String name = scanner.nextLine();
+	System.out.println("Your name is "+name);
 	}
 
 }
